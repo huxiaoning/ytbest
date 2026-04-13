0073492d03fde46ea83e3ef6762e8c18f3ff80d9
huxiaoningsworld <huxiaoningsworld@sina.com>
2026-04-13 16:50:31 +0800

    ytlist

--- ytlist.ps1
+++ ytlist.ps1
@@ -25,8 +25,24 @@ $subtile = @('--embed-subs', '--write-subs', '--write-auto-subs', '--convert-sub
 # 脚本工作目录
 $cwd = $PWD.Path
 
-Write-Host "🚀 正在下载最高画质视频..." -ForegroundColor Cyan
 
+Write-Host "🚀 计算播放列表长度..." -ForegroundColor Cyan
+$count=((yt-dlp --cookies-from-browser chrome --flat-playlist --print "%(id)s" $Url) 2>$null | Measure-Object -Line).Lines
+Write-Host "🚀 计算播放列表长度为：$count..." -ForegroundColor Cyan
+
+if($count -lt 10){
+    $prefix="%(autonumber)d"
+} elseif ($count -lt 100) {
+    $prefix="%(autonumber)02d"
+} elseif ($count -lt 1000) {
+    $prefix="%(autonumber)03d"
+} else {
+    $prefix="%(autonumber)s"
+}
+Write-Host "🚀 剧集前缀为：$prefix..." -ForegroundColor Cyan
+
+
+Write-Host "🚀 正在下载最高画质视频..." -ForegroundColor Cyan
 # %(autonumber)03d - 指定序号格式为 001 002 003 ..
 yt-dlp --external-downloader aria2c --external-downloader-args "-x 16 -s 16 -k 1M -c" --yes-playlist --playlist-reverse -o "$cwd\%(playlist_title)s\%(autonumber)d.%(title)s.%(ext)s" -I "1:" --cookies-from-browser chrome --no-check-certificate --compat-options no-youtube-unavailable-videos -f best/bestvideo*+bestaudio $subtile --embed-metadata $Url
 
